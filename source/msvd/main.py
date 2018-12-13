a8777685bbc134a054ac8bf9c6e150cb42135f68
Duy Trung TRAN <tranduytrung@outlook.com>
2018-12-13 21:23:38 +0100

    msvd remove duplicate line

--- source/msvd/main.py
+++ source/msvd/main.py
@@ -7,7 +7,8 @@ def main():
     eng_pd = raw_pd[raw_pd["Language"] == "English"]
     eng_pd["FileName"] = eng_pd.apply(lambda x: "{}_{}_{}".format(x["VideoID"], x["Start"], x["End"]), axis=1)
     
-    selected_np = eng_pd[["FileName", "Description"]].values
+    selected = eng_pd[["FileName", "Description"]]
+    selected_np = selected.drop_duplicates().values
     with open("../../data/msvd_video_caps.pkl", "wb") as f:
         pickle.dump(selected_np, f)
     print("done")
